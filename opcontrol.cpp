50deb222e827871c1c60d68604310eee55ff00fd
bleh0.5 <qinxiaoying@gmail.com>
2019-07-09 17:28:46 -0500

    fixed stuff

--- opcontrol.cpp
+++ opcontrol.cpp
@@ -16,7 +16,7 @@ using namespace okapi;
 //testing something
 bool onoff = false;
  
-//ports
+//ports/encoders
 pros::Motor left_wheels (LEFT_WHEELS_PORT);
 pros::Motor left_wheels_2 (LEFT_WHEELS_PORT_2,true);
 pros::Motor right_wheels (RIGHT_WHEELS_PORT, true);
@@ -25,6 +25,7 @@ pros::Motor intake_L (INTAKE_PORT_L);
 pros::Motor intake_R (INTAKE_PORT_R);
 pros::Motor outtake_2 (OUTTAKE_PORT_2,MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor outtake (OUTTAKE_PORT,MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+IntegratedEncoder enc = new IntegratedEncode(outtake);
  
 //budget motion profiling???
 std::vector<int> right_motor_movement_log={};
@@ -59,31 +60,34 @@ void stopIntake(){
 }
 void stopOuttake(){
   outtake.move(0);
+  outtake_2.move(0);
   outtake_motor_movement_log.push_back(0);
 }
-void outtake_macro(bool dir){//false for reverse
-  IntegratedEncoder enc = IntegratedEncoder(outtake);
-  enc.reset();
-  if(dir){
-    while(enc.get()<=OUTTAKE_ENCODER_TICKS){
+boolean outtake_macro(boolean state){//false for reverse
+  if(state==1){
+    if(enc.get()<OUTTAKE_ENCODER_TICKS){
       outtake.move(OUTTAKE_SPEED);
       outtake_2.move(-OUTTAKE_SPEED);
       outtake_motor_movement_log.push_back(OUTTAKE_SPEED);
     }
+    else{
+      stopOuttake();
+    }
   }
-  else{
-    while(enc.get()>=-OUTTAKE_ENCODER_TICKS){
+  if(state==0){
+    if(enc.get()>0){
       outtake.move(-OUTTAKE_SPEED);
       outtake_2.move(OUTTAKE_SPEED);
       outtake_motor_movement_log.push_back(-OUTTAKE_SPEED);
     }
+    else{
+      stopOuttake();
+    }
   }
-  outtake.move(0);
-  outtake_2.move(0);
-  outtake_motor_movement_log.push_back(-0);
 }
 void opcontrol() {
   int tick = 0;
+  enc.reset();
   pros::Controller master (CONTROLLER_MASTER);
  
   outtake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -94,57 +98,53 @@ void opcontrol() {
   left_wheels_2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
   right_wheels_2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
   while (true) {
-    //DRIVE (TANK)
-    if(!onoff){
-      /*float left=(master.get_analog(ANALOG_LEFT_Y)*SPEED_COEFFICIENT);
-      float right=(master.get_analog(ANALOG_RIGHT_Y)*SPEED_COEFFICIENT);
-      left_wheels.move(left);
-      right_wheels.move(right);
-      left_wheels_2.move(left);
-      right_wheels_2.move(right);
-      left_motor_movement_log.push_back(left);
-      right_motor_movement_log.push_back(right);
-      */
-      int power = controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y);
-      int turn = controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_X);
-      int left = power + turn;
-      int right = power - turn;
-      right *= -1; // This reverses the right motor
-      motor_move(LEFT_WHEELS_PORT, left);
-      motor_move(RIGHT_WHEELS_PORT, right);
-      left_motor_movement_log.push_back(left);
-      right_motor_movement_log.push_back(right);
-      //SLOW MODE CONTROL
-      if (master.get_digital(DIGITAL_L1)) {
-        SPEED_COEFFICIENT=SPEED_SLOW;
-      }
-      else  {
-        SPEED_COEFFICIENT=SPEED_FAST;//i hear that 126 > 127
-      }
-      //INTAKE CONTROL
-      if (master.get_digital(DIGITAL_R1)) {
-        moveIntake(true);
-      }
-      else if (master.get_digital(DIGITAL_R2)) {
-        moveIntake(false);
-      }
-      else {
-        stopIntake();
-      }
-      //OUTTAKE SYSTEM
-      IntegratedEncoder enc = IntegratedEncoder(outtake);
-      if(master.get_digital(DIGITAL_X)){
-        //moveOuttake(true);//controlled outtake
-        outtake_macro(true);
-      }
-      else if(master.get_digital(DIGITAL_UP)){
-        //moveOuttake(false);
-        outtake_macro(false);
-      }
-      else{
-        stopOuttake();
-      }
+    DRIVE (TANK)
+    /*float left=(master.get_analog(ANALOG_LEFT_Y)*SPEED_COEFFICIENT);
+    float right=(master.get_analog(ANALOG_RIGHT_Y)*SPEED_COEFFICIENT);
+    left_wheels.move(left);
+    right_wheels.move(right);
+    left_wheels_2.move(left);
+    right_wheels_2.move(right);
+    left_motor_movement_log.push_back(left);
+    right_motor_movement_log.push_back(right);
+    */
+    int power = controller_get_analog(CONTROLLER_MASTER, ANALOG_LEFT_Y);
+    int turn = controller_get_analog(CONTROLLER_MASTER, ANALOG_RIGHT_X);
+    int left = (power + turn)*SPEED_COEFFICIENT;
+    int right = (power - turn)*SPEED_COEFFICIENT;
+    right *= -1; // This reverses the right motor
+    motor_move(LEFT_WHEELS_PORT, left);
+    motor_move(RIGHT_WHEELS_PORT, right);
+    left_motor_movement_log.push_back(left);
+    right_motor_movement_log.push_back(right);
+    //SLOW MODE CONTROL
+    if (master.get_digital(DIGITAL_L1)) {
+      SPEED_COEFFICIENT=SPEED_SLOW;
     }
+    else  {
+      SPEED_COEFFICIENT=SPEED_FAST;//i hear that 126 > 127
+    }
+    //INTAKE CONTROL
+    if (master.get_digital(DIGITAL_R1)) {
+      moveIntake(true);
+    }
+    else if (master.get_digital(DIGITAL_R2)) {
+      moveIntake(false);
+    }
+    else {
+      stopIntake();
+    }
+    //OUTTAKE SYSTEM
+    bool state = false;
+    if(master.get_digital(DIGITAL_X)){
+      //moveOuttake(true);//controlled outtake
+      state=1;
+    }
+    else if(master.get_digital(DIGITAL_UP)){
+      //moveOuttake(false);
+      state=0;
+    }
+    outtake_macro(state);
     if(master.get_digital(DIGITAL_Y)&&tick<left_motor_movement_log.size()){
       left_wheels.move(left_motor_movement_log[tick]);
       right_wheels.move(right_motor_movement_log[tick]);
@@ -155,11 +155,7 @@ void opcontrol() {
       outtake.move(outtake_motor_movement_log[tick]);
       outtake_2.move(-outtake_motor_movement_log[tick]);
       tick++;
-      printf("%d\n",tick);
     }
-    else if (master.get_digital(DIGITAL_Y)) {
-      onoff^=1;
-    }
-    pros::delay(5);
+    pros::delay(10);
   }
 }
