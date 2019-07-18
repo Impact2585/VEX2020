28b5bbe6adfc7312306172401f9bd7601c5c2ee9
bleh0.5 <qinxiaoying@gmail.com>
2019-07-17 19:04:09 -0500

    Update autonomous.cpp

--- autonomous.cpp
+++ autonomous.cpp
@@ -120,7 +120,7 @@ void autonomous() {
     profileController1.setTarget("intake1");
       profileController1.waitUntilSettled();
       profileController2.generatePath({
-        Point{0_ft, c*-0.6_ft, 0_deg},
+        Point{0_ft, c*-1.2_ft, 0_deg},
         Point{1.9_ft, 0_ft, 0_deg}},
         "back1" // Profile name
       );
@@ -136,7 +136,7 @@ void autonomous() {
     //stopintake
     //turn 135 degrees
     chassis.setMaxVelocity(100);
-    chassis.turnAngle(c*199);
+    chassis.turnAngle(c*206);
     pros::delay(100);
      
     chassis.setMaxVelocity(200);
